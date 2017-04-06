26a5d3e639f4da56967e6e16bb35f4abe539e9a1
EternalSoap <fran.goldner@gmail.com>
2017-04-06 13:56:57 +0200

    Initial commit

---
+++ src/main/java/com/goldner/telegram/channelLikeBot/App.java
@@ -0,0 +1,14 @@
+package com.goldner.telegram.channelLikeBot;
+
+/**
+ * Telegram channel like bot
+ * Replies to all the image posts in a channel with an inline keyboard that allows liking/disliking images
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/goldner/telegram/channelLikeBot/channelLikeBot.java
@@ -0,0 +1,23 @@
+package com.goldner.telegram.channelLikeBot;
+
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+/**
+ * Created by fran on 06.04.17..
+ */
+public class channelLikeBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "channelLike_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "337076832:AAGpnBuketcGI3DsvuCcGPBcGenpD6cX2pw";
+    }
+}
